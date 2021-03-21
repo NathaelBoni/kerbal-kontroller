491079e87d0c376c6b7c5ea2bf558ea419e82330
Nathael <nathaelboni@gmail.com>
2021-03-21 18:08:30 -0300

    Add Arduino Driver package, create PoC

--- src/KerbalKontroller/Program.cs
+++ src/KerbalKontroller/Program.cs
@@ -1,4 +1,7 @@
-﻿using KRPC.Client.Services.SpaceCenter;
+﻿using ArduinoDriver.SerialProtocol;
+using ArduinoUploader.Hardware;
+using KRPC.Client.Services.SpaceCenter;
+using System;
 
 namespace KerbalKontroller
 {
@@ -9,7 +12,20 @@ namespace KerbalKontroller
             var connection = new KRPC.Client.Connection("ksp");
             var vessel = connection.SpaceCenter().ActiveVessel;
 
-            vessel.Control.Pitch = 0.5f;
+            var driver = new ArduinoDriver.ArduinoDriver(ArduinoModel.Leonardo, true);
+
+            var valX = driver.Send(new AnalogReadRequest(0)).PinValue;
+
+            vessel.Control.Pitch = AnalogConversor(valX);
+        }
+
+        private static float AnalogConversor(int analogValue)
+        {
+            var deadZone = 0.018f;
+            var convertedValue = (analogValue / 511.5f) - 1;
+
+            if (Math.Abs(convertedValue) < deadZone) return 0;
+            return convertedValue;
         }
     }
 }
