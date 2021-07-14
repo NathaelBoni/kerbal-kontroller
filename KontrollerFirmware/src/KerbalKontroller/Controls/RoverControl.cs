3d1b4512529822c5d350e6c626d9c604182e439e
Nathael Boni <nathaelboni@gmail.com>
2021-07-14 19:57:53 -0300

    Remove debounce from static classes

--- KontrollerFirmware/src/KerbalKontroller/Controls/RoverControl.cs
+++ KontrollerFirmware/src/KerbalKontroller/Controls/RoverControl.cs
@@ -32,7 +32,17 @@ namespace KerbalKontroller.Controls
 
             ControlHelper.SetSASMode(hardwareClient, kspClient);
             ControlHelper.SetToggleSwitches(hardwareClient, kspClient);
-            ControlHelper.ActionGroup(debounce, kspClient);
+
+            if (debounce.GetAction1ButtonState()) kspClient.ActivateAction(1);
+            if (debounce.GetAction2ButtonState()) kspClient.ActivateAction(2);
+            if (debounce.GetAction3ButtonState()) kspClient.ActivateAction(3);
+            if (debounce.GetAction4ButtonState()) kspClient.ActivateAction(4);
+            if (debounce.GetAction5ButtonState()) kspClient.ActivateAction(5);
+            if (debounce.GetAction6ButtonState()) kspClient.ActivateAction(6);
+            if (debounce.GetAction7ButtonState()) kspClient.ActivateAction(7);
+            if (debounce.GetAction8ButtonState()) kspClient.ActivateAction(8);
+            if (debounce.GetAction9ButtonState()) kspClient.ActivateAction(9);
+            if (debounce.GetAction10ButtonState()) kspClient.ActivateAction(10);
 
             if (debounce.GetStageButtonState()) kspClient.Stage();
 
